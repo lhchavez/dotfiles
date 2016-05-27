f0aefa3a28aecf48901ee1fc4d9e62e1a796a77b
lhchavez <lhchavez@lhchavez.com>
2016-05-26 21:28:43 -0700

    Fix python3 support for pymatcher

--- vim/build.sh
+++ vim/build.sh
@@ -16,8 +16,7 @@ cd $ROOT/third_party/vim
 make distclean
 LDFLAGS='-Wl,-E  -Wl,-Bsymbolic-functions -fPIE -pie -Wl,-z,relro -Wl,-z,now -Wl,--as-needed' \
 	CFLAGS='-Wdate-time  -g -O2 -fPIE -fstack-protector-strong -Wformat -Werror=format-security -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=1' \
-	./configure --enable-pythoninterp=yes --enable-python3interp=yes --enable-multibyte --enable-rubyinterp=yes \
-	--with-python-config-dir=/usr/lib/python3/config --enable-tclinterp=yes --enable-perlinterp=yes --enable-luainterp=yes \
-	--enable-cscope --prefix=/usr --enable-gui=gnome2 --enable-gnome-check
+	./configure --enable-python3interp=yes --enable-multibyte --enable-rubyinterp=yes --with-python-config-dir=/usr/lib/python3/config \
+	--enable-tclinterp=yes --enable-perlinterp=yes --enable-luainterp=yes --enable-cscope --prefix=/usr --enable-gui=gnome2 --enable-gnome-check
 make -j6
 sudo make install
